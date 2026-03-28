5a1f477d8c273d4909dc8b5ac29b5b1da9d16d17
Júlio Magalhães <jcmagalhaes301@gmail.com>
2026-03-27 21:39:25 -0300

    [FIAP-120] feat: tela de dashboard

--- src/app/features/dashboard/containers/dashboard/dashboard.ts
+++ src/app/features/dashboard/containers/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardHeader } from './../../components/dashboard-header/dashboard-header';
+import { Button } from "../../../../shared/components/button/button";
+import { ProgressBar } from "../../../../shared/components/progress-bar/progress-bar";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DashboardHeader, Button, ProgressBar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
